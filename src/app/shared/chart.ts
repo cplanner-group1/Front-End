b957cae851f70004a8c253b3b2e17601dd951a8d
Ashkan Vedadi <ashkan.vedadi@live.com>
2021-05-06 15:03:37 +0430

    charts search

--- src/app/shared/chart.ts
+++ src/app/shared/chart.ts
@@ -10,7 +10,29 @@ export class Chart{
     university: string;
 
     date: string; // date added
+
+    courses: CourseTrack[];
 }
+export class Course{
+    id: number;
+    title: string;
+    suggestedPrerequisites: number[]; // suggested list of id of courses which prerequisite of this course
+}
+export class CourseTrack{
+    course: Course;
+    prerequisites: number[]; // list of id of courses which prerequisite of this course
+    status: number; 
+        // 0 --> feature
+        // 1 --> doing
+        // 2 --> done 
+    grade: number;
+}
+
+// intorie ke chart shamele majmoe ei courseTrack hast va har courseTrack marbot b yek farde khase ama course global e 
+// dar course age title motefavet bood id jadid bde behesh 
+// prerequisites ha id course haro zakhire mikonee k pish niaz ast chon momken ast pish niaz dr daneshgah hae mokhtlf farq kne to courseTrack ovordm
+// suggestedPrerequisites vase ine k hame halat hae ta hala por shde ro dshte bashim k vaqti add krd yeho ezfe kne o oni k nis ro hazf kne
+
 
 export const Charts: Chart[] = [
     {
@@ -23,7 +45,8 @@ export const Charts: Chart[] = [
         study: "مکانیک",
         university:"دانشگاه امیرکبیر",
 
-        date: "2020/05/12"
+        date: "2020/05/12",
+        courses: null
     },
     {
         id: 2 ,
@@ -35,6 +58,7 @@ export const Charts: Chart[] = [
         study: "برق",
         university:"دانشگاه تهران",
 
-        date: "2021/10/12"
+        date: "2021/10/12",
+        courses:null
     }
 ]
