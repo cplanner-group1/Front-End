a250659bb69d28374877af17f65767e07916b8f1
Par <p@Ps-MacBook-Pro.local>
2021-05-07 18:08:29 +0430

    signup page password visibility toggle

--- src/app/signin-signup/signin-signup.component.ts
+++ src/app/signin-signup/signin-signup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import * as $ from 'jquery';;
+
 
 @Component({
   selector: 'app-signin-signup',
@@ -12,7 +15,6 @@ export class SigninSignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
-
   panelPositionAdd(){
     let container = document.getElementById('container');
     container?.classList.add("right-panel-active");
@@ -21,24 +23,126 @@ export class SigninSignupComponent implements OnInit {
     let container = document.getElementById('container');
     container?.classList.remove("right-panel-active");
   }
-  /*
+  
+  @ViewChild('passwordInput') input: ElementRef;
+
   passwordVisibilityToggle() {
 
-    var mdp = document.getElementById("password");
     var passwordEye = document.getElementById("password-eye");
   
-    if (mdp?.nativeElement.setAttribute === 'password') {
-      // - Le type de l'input type=password devient type=text
-      mdp.type = "text";
-      // - L'icone change et devient password-eye.on
+    if ((this.input.nativeElement as HTMLInputElement).type === 'password')
+    {
+      (this.input.nativeElement as HTMLInputElement).type = "text";
       passwordEye?.classList.remove("off");
       passwordEye?.classList.add("on");
      } else {
-       mdp.type = "password";
+      (this.input.nativeElement as HTMLInputElement).type = "password";
        passwordEye?.classList.remove("on");
       passwordEye?.classList.add("off");
      }
-  } */
+  } 
+
+  /*FORGOT PASSWORD*/
+
+  /*
+  $(".modal").each( function(){
+    $(this).wrap('<div class="overlay1"></div>')
+  });
+  
+  /*$(".open-modal").on('click', function(e)*/
+  openForgotPassword (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation;
+    
+    var $this = $(this),
+        modal = $($this).data("modal");
+    
+    $(modal).parents(".overlay1").addClass("open");
+    setTimeout( function(){
+      $(modal).addClass("open");
+    }, 350);
+    
+    $(document).on('click', function(e){
+      var target = $(e.target);
+      
+      if ($(target).hasClass("overlay1")){
+        $(target).find(".modal").each( function(){
+          $(this).removeClass("open");
+        });
+        setTimeout( function(){
+          $(target).removeClass("open");
+        }, 350);
+      }
+      
+    });
+    
+  };
+  
+  /*$(".close-modal").on('click', function(e)*/
+  closeForgotPassword(e){
+    e.preventDefault();
+    e.stopImmediatePropagation;
+    
+    var $this = $(this),
+        modal = $($this).data("modal");
+    
+    $(modal).removeClass("open");
+    setTimeout( function(){	
+      $(modal).parents(".overlay").removeClass("open");
+    }, 350);
+    
+  };
+
   title = 'padis';
 
 }
+
+
+/* 
+PASSWORD POPUP CODE
+    $(".modal").each( function(){
+      $(this).wrap('<div class="overlay1"></div>')
+    });
+    
+    $(".open-modal").on('click', function(e){
+      e.preventDefault();
+      e.stopImmediatePropagation;
+      
+      var $this = $(this),
+          modal = $($this).data("modal");
+      
+      $(modal).parents(".overlay1").addClass("open");
+      setTimeout( function(){
+        $(modal).addClass("open");
+      }, 350);
+      
+      $(document).on('click', function(e){
+        var target = $(e.target);
+        
+        if ($(target).hasClass("overlay1")){
+          $(target).find(".modal").each( function(){
+            $(this).removeClass("open");
+          });
+          setTimeout( function(){
+            $(target).removeClass("open");
+          }, 350);
+        }
+        
+      });
+      
+    });
+    
+    $(".close-modal").on('click', function(e){
+      e.preventDefault();
+      e.stopImmediatePropagation;
+      
+      var $this = $(this),
+          modal = $($this).data("modal");
+      
+      $(modal).removeClass("open");
+      setTimeout( function(){	
+        $(modal).parents(".overlay").removeClass("open");
+      }, 350);
+      
+    });	
+*/
