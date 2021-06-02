f18cf1f653e833e49d1a1926fe6c5e2d300063bf
Ashkan Vedadi <ashkan.vedadi@live.com>
2021-06-02 20:52:57 +0430

    course modal

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectTypeNumberValue } from '../shared/select';
 
 @Component({
   selector: 'app-add-course',
@@ -9,7 +10,24 @@ export class AddCourseComponent implements OnInit {
 
   constructor() { }
 
+  datePickerConfig = {
+    drops: 'down'
+  };
+  dayWeekType: SelectTypeNumberValue[] = [
+    {value:0,viewValue:'شنبه'},
+    {value:1,viewValue:'یک شنبه'},
+    {value:2,viewValue:'دو شنبه'},
+    {value:3,viewValue:'سه شنبه'},
+    {value:4,viewValue:'چهار شنبه'},
+    {value:5,viewValue:'پنج شنبه'},
+    {value:6,viewValue:'جمعه'}
+  ];
+  day: number=0;
+  startTime: string='00:00:00';
+  endTime: string='02:00:00';
+  examTime: string='';
   ngOnInit(): void {
   }
+  
 
 }
